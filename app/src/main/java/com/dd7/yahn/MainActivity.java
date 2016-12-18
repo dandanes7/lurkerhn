0ec0dd762aea792a707529d1606aa5d05e7efe19
Dan Danes <dandanes7@gmail.com>
2016-12-18 15:00:07 +0200

    add navigation drawer

--- app/src/main/java/com/dd7/yahn/MainActivity.java
+++ app/src/main/java/com/dd7/yahn/MainActivity.java
@@ -2,13 +2,18 @@ package com.dd7.yahn;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -16,9 +21,9 @@ import android.widget.ListView;
 import android.widget.Toast;
 import com.dd7.yahn.adapter.ClickListener;
 import com.dd7.yahn.adapter.StoryCardAdapter;
-import com.dd7.yahn.rest.model.Item;
-import com.dd7.yahn.rest.client.HackerNewsApiClient;
 import com.dd7.yahn.rest.client.ClientFactory;
+import com.dd7.yahn.rest.client.HackerNewsApiClient;
+import com.dd7.yahn.rest.model.Item;
 import com.dd7.yahn.service.PreferenceService;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
@@ -26,7 +31,7 @@ import rx.schedulers.Schedulers;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
 
     private final String[] CATEGORIES = {"Saved Stories", "Settings"};
     private int MAX_STORIES_TO_DISPLAY = 50;
@@ -34,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
     private Context mContext;
     private SwipeRefreshLayout mSwipeRefreshLayout;
     private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private String mActivityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +62,29 @@ public class MainActivity extends AppCompatActivity {
         final StoryCardAdapter mStoryCardAdapter = new StoryCardAdapter(this);
         mStoryCardAdapter.setOnItemClickListener(new ItemClickListener());
         mRecyclerView.setAdapter(mStoryCardAdapter);
+
+        mActivityTitle = getTitle().toString();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Navigation!");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
 
         mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
         mSwipeRefreshLayout.setOnRefreshListener(() -> {
@@ -111,7 +141,40 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public class DrawerItemClickListener implements ListView.OnItemClickListener{
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        // Activate the navigation drawer toggle
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public class DrawerItemClickListener implements ListView.OnItemClickListener {
 
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -120,7 +183,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(mContext, Class.forName(name));
                 startActivity(intent);
             } catch (ClassNotFoundException e) {
-                Toast.makeText(mContext, "Could not find activity "+name, Toast.LENGTH_SHORT).show();
+                Toast.makeText(mContext, "Could not find activity " + name, Toast.LENGTH_SHORT).show();
             }
         }
     }
